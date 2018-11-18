6a195342033e3542f260c2d9d2930bb1c7bfe643
brianfhilip <42954432+brianfhilip@users.noreply.github.com>
2018-11-18 18:36:11 -0500

    actualizacion ventanaVentas

---
+++ sipe/ventas.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace sipe
+{
+    partial class Facturacion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVenta = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblVendedor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumCedula = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.lblCodigoProducto = new System.Windows.Forms.Label();
+            this.lblCanttidadExistente = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCantidadAVender = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.rdbEfectivo = new System.Windows.Forms.RadioButton();
+            this.rbtTarjeta = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblVenta
+            // 
+            this.lblVenta.AutoSize = true;
+            this.lblVenta.Location = new System.Drawing.Point(30, 24);
+            this.lblVenta.Name = "lblVenta";
+            this.lblVenta.Size = new System.Drawing.Size(59, 13);
+            this.lblVenta.TabIndex = 0;
+            this.lblVenta.Text = "NIT. Venta";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(434, 24);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(37, 13);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // lblVendedor
+            // 
+            this.lblVendedor.AutoSize = true;
+            this.lblVendedor.Location = new System.Drawing.Point(228, 24);
+            this.lblVendedor.Name = "lblVendedor";
+            this.lblVendedor.Size = new System.Drawing.Size(53, 13);
+            this.lblVendedor.TabIndex = 2;
+            this.lblVendedor.Text = "Vendedor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "N° Cedula Cliente";
+            // 
+            // txtNumCedula
+            // 
+            this.txtNumCedula.Location = new System.Drawing.Point(108, 71);
+            this.txtNumCedula.Name = "txtNumCedula";
+            this.txtNumCedula.Size = new System.Drawing.Size(114, 20);
+            this.txtNumCedula.TabIndex = 4;
+            this.txtNumCedula.TextChanged += new System.EventHandler(this.txtNumCedula_TextChanged);
+            this.txtNumCedula.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumCedula_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre Cliente";
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Location = new System.Drawing.Point(313, 71);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(162, 20);
+            this.txtNombreCliente.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Producto";
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(108, 110);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(367, 20);
+            this.txtProducto.TabIndex = 8;
+            this.txtProducto.TextChanged += new System.EventHandler(this.txtProducto_TextChanged);
+            this.txtProducto.Enter += new System.EventHandler(this.txtProducto_Enter);
+            this.txtProducto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtProducto_KeyDown);
+            this.txtProducto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtProducto_KeyPress);
+            this.txtProducto.Leave += new System.EventHandler(this.txtProducto_Leave);
+            this.txtProducto.Validated += new System.EventHandler(this.txtProducto_Validated);
+            // 
+            // lblCodigoProducto
+            // 
+            this.lblCodigoProducto.AutoSize = true;
+            this.lblCodigoProducto.Location = new System.Drawing.Point(48, 163);
+            this.lblCodigoProducto.Name = "lblCodigoProducto";
+            this.lblCodigoProducto.Size = new System.Drawing.Size(102, 13);
+            this.lblCodigoProducto.TabIndex = 9;
+            this.lblCodigoProducto.Text = "Codigo del producto";
+            // 
+            // lblCanttidadExistente
+            // 
+            this.lblCanttidadExistente.AutoSize = true;
+            this.lblCanttidadExistente.Location = new System.Drawing.Point(167, 163);
+            this.lblCanttidadExistente.Name = "lblCanttidadExistente";
+            this.lblCanttidadExistente.Size = new System.Drawing.Size(95, 13);
+            this.lblCanttidadExistente.TabIndex = 10;
+            this.lblCanttidadExistente.Text = "Cantidad Existente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Cantidad a vender";
+            // 
+            // txtCantidadAVender
+            // 
+            this.txtCantidadAVender.Location = new System.Drawing.Point(372, 160);
+            this.txtCantidadAVender.Name = "txtCantidadAVender";
+            this.txtCantidadAVender.Size = new System.Drawing.Size(103, 20);
+            this.txtCantidadAVender.TabIndex = 12;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(381, 236);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // rdbEfectivo
+            // 
+            this.rdbEfectivo.AutoSize = true;
+            this.rdbEfectivo.Location = new System.Drawing.Point(294, 246);
+            this.rdbEfectivo.Name = "rdbEfectivo";
+            this.rdbEfectivo.Size = new System.Drawing.Size(64, 17);
+            this.rdbEfectivo.TabIndex = 14;
+            this.rdbEfectivo.TabStop = true;
+            this.rdbEfectivo.Text = "Efectivo";
+            this.rdbEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // rbtTarjeta
+            // 
+            this.rbtTarjeta.AutoSize = true;
+            this.rbtTarjeta.Location = new System.Drawing.Point(294, 269);
+            this.rbtTarjeta.Name = "rbtTarjeta";
+            this.rbtTarjeta.Size = new System.Drawing.Size(58, 17);
+            this.rbtTarjeta.TabIndex = 15;
+            this.rbtTarjeta.TabStop = true;
+            this.rbtTarjeta.Text = "Tarjeta";
+            this.rbtTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Tipo de pago";
+            // 
+            // Facturacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 314);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rbtTarjeta);
+            this.Controls.Add(this.rdbEfectivo);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCantidadAVender);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCanttidadExistente);
+            this.Controls.Add(this.lblCodigoProducto);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumCedula);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblVendedor);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblVenta);
+            this.Name = "Facturacion";
+            this.Text = "Facturacion";
+            this.Load += new System.EventHandler(this.Facturacion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVenta;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblVendedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumCedula;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.Label lblCodigoProducto;
+        private System.Windows.Forms.Label lblCanttidadExistente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCantidadAVender;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.RadioButton rdbEfectivo;
+        private System.Windows.Forms.RadioButton rbtTarjeta;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ sipe/ventas.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace sipe
+{
+    public partial class Facturacion : Form
+    {
+
+
+        public Facturacion()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void txtNumCedula_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumCedula_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                try
+                {
+                    MySqlCommand miSentencia = new MySqlCommand("buscarCliente", conexion.crearConexion());
+                    miSentencia.CommandType = CommandType.StoredProcedure;
+
+                    miSentencia.Parameters.AddWithValue("id", txtNumCedula.Text);
+                    MySqlDataReader reader = miSentencia.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+
+
+
+                        if (reader.GetString(0).Equals("0"))
+                        {
+
+                            MessageBox.Show("El cliente no existe");
+
+                        }
+                        else
+                        {
+                            txtNombreCliente.Text = reader.GetString(1) + " " + reader.GetString(2);
+
+                        }
+
+
+                    }
+
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private void txtProducto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+
+           /* if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+
+                try
+                {
+                    MySqlCommand miSentencia = new MySqlCommand("MostrarIDproducto", ConexionSipe.Conexion());
+                    miSentencia.CommandType = CommandType.StoredProcedure;
+
+                    miSentencia.Parameters.AddWithValue("nombre", txtProducto.Text);
+                    MySqlDataReader reader = miSentencia.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+
+
+                        lblCodigoProducto.Text = reader.GetString(0);
+
+
+                    }
+
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }*/
+        }
+
+        private void txtProducto_TextChanged(object sender, EventArgs e)
+        {
+            this.Focus();
+            if (txtProducto.Focus())
+            {
+                
+
+            }
+        }
+
+        protected override void OnLostFocus(EventArgs e)
+        {
+
+            base.OnLostFocus(e);
+        }
+
+        private void ElFoco_LostFocus(object sender, EventArgs e)
+        {
+           
+     
+        }
+
+        private void txtProducto_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void agregar()
+        {
+
+        }
+
+        private void txtProducto_KeyDown(object sender, KeyEventArgs e)
+        {
+            agregar();
+
+        }
+
+        private void txtProducto_Leave(object sender, EventArgs e)
+        {
+            agregar();
+        }
+
+        private void txtProducto_Validated(object sender, EventArgs e)
+        {
+
+            try
+            {
+                MySqlCommand miSentencia = new MySqlCommand("MostrarIDproducto", conexion.crearConexion());
+                MySqlCommand miSentencia2 = new MySqlCommand("mostrarCantidadProducto", conexion.crearConexion());
+                miSentencia.CommandType = CommandType.StoredProcedure;
+                miSentencia2.CommandType = CommandType.StoredProcedure;
+
+                miSentencia.Parameters.AddWithValue("nombre", txtProducto.Text);
+                miSentencia2.Parameters.AddWithValue("CantProdu", txtProducto.Text);
+
+                MySqlDataReader reader = miSentencia.ExecuteReader();
+                MySqlDataReader reader1 = miSentencia2.ExecuteReader();
+
+
+                while (reader.Read () )
+                {
+                    
+                    lblCodigoProducto.Text = reader.GetString(0);
+                    
+                }
+                while (reader1.Read())
+                {
+
+                    lblCanttidadExistente.Text = reader1.GetString(0);
+
+                }
+
+
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            
+
+
+            
+        }
+
+        private void Facturacion_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
